82afa3a1ff3f5dfc6ba7a6f9043383eea40246ab
Hunor Lakatos <lakatoshunor9716@gmail.com>
2023-02-04 15:39:24 +0100

    multi input main menu

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
@@ -15,5 +16,20 @@ public class MainMenu : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void OnPlaySelected()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void OnCreditsSelected()
+    {
+        Debug.Log("Credits");
+    }
+
+    public void OnQuitSelected()
+    {
+        Application.Quit();
     }
 }
