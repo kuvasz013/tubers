cfbf4e3e04b210d6df585507f2a74cc18846d72e
Kuvasz <kuvasz013@gmail.com>
2023-02-05 10:05:19 +0100

    FEAT: Spawning logic

---
+++ Assets/Scripts/Game/PlayerConfig.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine.InputSystem;
+
+public struct PlayerConfig {
+    public int playerID;
+    public string controlScheme;
+    public InputDevice inputDevice;
+    public TuberType tuberType;
+    public PlayerInput playerInput;
+}
+
+public enum TuberType
+{
+    Potato,
+    Carrot,
+    Beet,
+    Scallion
+}
