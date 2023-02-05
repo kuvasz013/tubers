dc612b7d7a93e6d78fe3ee74de828994e46a9a0e
Hunor Lakatos <lakatoshunor9716@gmail.com>
2023-02-05 18:29:30 +0100

    changed endgame texts

--- Assets/Scripts/UI/EndGame.cs
+++ Assets/Scripts/UI/EndGame.cs
@@ -16,28 +16,28 @@ public class EndGame : MonoBehaviour
         if (manager != null) {
             winner = TuberType.NONE;
         }
-
+        
         switch (winner)
         {
             case TuberType.Potato:
-                title.text = "Potato";
+                title.text = "You’ve made it, Pot Ato! You are a free veggie now!";
                 veggiePics[0].SetActive(true);
                 break;
             case TuberType.Carrot:
-                title.text = "Carrot";
+                title.text = "You’ve made it, C’Arrot! You are a free veggie now!";
                 veggiePics[1].SetActive(true);
                 break;
             case TuberType.Beet:
-                title.text = "Beet";
+                title.text = "You’ve made it, B.T. Root! You are a free veggie now!";
                 veggiePics[2].SetActive(true);
                 break;
             case TuberType.Scallion:
-                title.text = "Leek";
+                title.text = "You’ve made it, L33k! You are a free veggie now!";
                 veggiePics[3].SetActive(true);
                 break;
             case TuberType.NONE:
             default:
-                title.text = "Grandma";
+                title.text = ":( You didn’t make it. But at least the borscht is perfect, and Grandma is happy.";
                 veggiePics[4].SetActive(true);
                 break;
         }
@@ -48,6 +48,6 @@ public class EndGame : MonoBehaviour
     private IEnumerator End()
     {
         yield return new WaitForSeconds(5f);
-        SceneManager.LoadScene(0);
+        //SceneManager.LoadScene(0);
     }
 }
